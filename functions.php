1603d2063eceb8a97b66e42926e9100da11c52c9
goldensniper88 <goldensniper88@hotmail.com>
2020-03-30 23:23:38 +0800

    second commit
    adding the home_template.php

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_filter('show_admin_bar', '__return_false');
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+   wp_enqueue_style( 'custom', get_stylesheet_directory_uri() . '/css/custom.css' );
+   wp_enqueue_style( 'responsive', get_stylesheet_directory_uri() . '/responsive.css' );
 }
---
+++ home_template.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * Template Name: Home Page
+     */
+?>
+
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.6.4/jquery.fullPage.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.6.4/jquery.fullPage.js"></script>
+</head>
+<body>
+    <div class = "nav">
+    </div>
+
+</body>
+</html>
